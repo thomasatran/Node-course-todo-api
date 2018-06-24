3ca16e83b9370b3487dca78e5555e234eb983b03
Thomas Tran <tat022@ucsd.edu>
2018-06-24 13:27:10 -0700

    test POST /todos route

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.post('/todos', (req, res)=>{
     todo.save().then((doc)=>{
         res.send(doc);
     },(e)=>{
-        res.send(e);
+        res.status(400).send(e);
     });
 });
 
@@ -27,5 +27,7 @@ app.post('/todos', (req, res)=>{
 app.listen(3000, ()=>{
     console.log('Started on port 3000');
 });
+
+module.exports = {app};
 
 
